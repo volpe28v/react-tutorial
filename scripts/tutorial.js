ace03fd8b7cce145481043f6b6ce170ab6fd247e
Kodama Naoki <kodama@trustia-no-MacBook-Pro.local>
2016-09-21 20:18:18 +0900

    turorial7

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -2,10 +2,52 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
+});
+
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function  () {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    rawMarkup: function(){
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return {__html: rawMarkup};
+    },
+
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
 });
 ReactDOM.render(
   <CommentBox />,

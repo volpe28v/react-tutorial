1a99dfbdc9cef1f917970facc78b0d73d47b484a
Kodama Naoki <kodama@trustia-no-MacBook-Pro.local>
2016-09-21 20:40:49 +0900

    tutorial14

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,9 +1,33 @@
 var CommentBox = React.createClass({
+    getInitialState: function(){
+        return {data: []};
+    },
+
+    loadCommentsFromServer: function(){
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    componentDidMount: function(){
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
     );
@@ -62,6 +86,6 @@ var data = [
 ];
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" pollInterval={2000}/>,
   document.getElementById('content')
 );
